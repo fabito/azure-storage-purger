eecf03d4a9f696b9d6c2d3c906139e23c3630ce9
Fabio Uechi <fabio.uechi@serko.com>
2020-04-30 23:14:23 +1200

    move generators to util

--- pkg/purger/generators.go
+++ pkg/purger/generators.go
@@ -4,38 +4,6 @@ import (
 	"sync"
 )
 
-// RepeatFn repeat with function
-func RepeatFn(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
-	valueStream := make(chan interface{})
-	go func() {
-		defer close(valueStream)
-		for {
-			select {
-			case <-done:
-				return
-			case valueStream <- fn():
-			}
-		}
-	}()
-	return valueStream
-}
-
-// Take Take
-func Take(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
-	takeStream := make(chan interface{})
-	go func() {
-		defer close(takeStream)
-		for i := 0; i < num; i++ {
-			select {
-			case <-done:
-				return
-			case takeStream <- <-valueStream:
-			}
-		}
-	}()
-	return takeStream
-}
-
 // FanIn FanIn
 func FanIn(done <-chan interface{}, channels ...<-chan *TableBatchResult) chan *TableBatchResult {
 	var wg sync.WaitGroup
@@ -66,40 +34,3 @@ func FanIn(done <-chan interface{}, channels ...<-chan *TableBatchResult) chan *
 
 	return multiplexedStream
 }
-
-// Repeat repeat
-func Repeat(done <-chan interface{}, values ...interface{}) <-chan interface{} {
-	valueStream := make(chan interface{})
-	go func() {
-		defer close(valueStream)
-		for {
-			for _, v := range values {
-				select {
-				case <-done:
-					return
-				case valueStream <- v:
-				}
-			}
-		}
-	}()
-	return valueStream
-}
-
-func toString(done <-chan interface{}, valueStream <-chan interface{}) <-chan string {
-	stringStream := make(chan string)
-	go func() {
-		defer close(stringStream)
-		for v := range valueStream {
-			select {
-			case <-done:
-				return
-			case stringStream <- v.(string):
-			}
-		}
-	}()
-	return stringStream
-}
-
-// func t() {
-// 	done := make(chan interface{})
-// 	defer close(done)
---
+++ pkg/util/generators.go
@@ -0,0 +1,101 @@
+package util
+
+import (
+	"sync"
+)
+
+// RepeatFn repeat with function
+func RepeatFn(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer close(valueStream)
+		for {
+			select {
+			case <-done:
+				return
+			case valueStream <- fn():
+			}
+		}
+	}()
+	return valueStream
+}
+
+// Take Take
+func Take(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valueStream:
+			}
+		}
+	}()
+	return takeStream
+}
+
+// FanIn FanIn
+func FanIn(done <-chan interface{}, channels ...<-chan interface{}) chan interface{} {
+	var wg sync.WaitGroup
+	multiplexedStream := make(chan interface{})
+
+	multiplex := func(c <-chan interface{}) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiplexedStream <- i:
+			}
+		}
+	}
+
+	// Select from all the channels
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiplex(c)
+	}
+
+	// Wait for all the reads to complete
+	go func() {
+		wg.Wait()
+		close(multiplexedStream)
+	}()
+
+	return multiplexedStream
+}
+
+// Repeat repeat
+func Repeat(done <-chan interface{}, values ...interface{}) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer close(valueStream)
+		for {
+			for _, v := range values {
+				select {
+				case <-done:
+					return
+				case valueStream <- v:
+				}
+			}
+		}
+	}()
+	return valueStream
+}
+
+func toString(done <-chan interface{}, valueStream <-chan interface{}) <-chan string {
+	stringStream := make(chan string)
+	go func() {
+		defer close(stringStream)
+		for v := range valueStream {
+			select {
+			case <-done:
+				return
+			case stringStream <- v.(string):
+			}
+		}
+	}()
+	return stringStream
+}
